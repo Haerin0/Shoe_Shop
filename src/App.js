b65e046d97e0d8d53a8b3f3323234a7304a99c5a
Haerin0 <parkhaerin@gmail.com>
2022-10-25 16:48:41 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import data from './data';
+
 
 function App() {
+
+  // let [shoeInfo, shoeState] = useState(2);
+  let [shoes] = useState(data)
+  console.log(shoes[0].title)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'></div>
+      <Container>
+        <Row>
+          <Products shoes={shoes} />
+          <Products shoes={shoes} />
+          <Products shoes={shoes} />
+        </Row>
+    </Container>
+
+
+
     </div>
   );
+}
+
+function Products(props){
+  return (
+    <Col sm>
+        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+        <h4>{props.shoes[0].title}</h4>
+        <p>상품 설명: {props.shoes[0].content}</p>
+        <p>가격: {props.shoes[0].price}</p>
+    </Col>
+  )
 }
 
 export default App;
