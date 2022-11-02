f7290aa80b262db4222ba82a5142a3e19331312a
Herin Park <parkhaerin@gmail.com>
2022-11-02 10:32:11 +0900

    starts from the beginning

--- src/App.js
+++ src/App.js
@@ -1,62 +1,51 @@
 import './App.css';
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import data from './data';
-import Detail from './pages/Detail';
-import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
-import axios from 'axios'
-import Cart from './pages/Cart';
-
 
 function App() {
-
-  // let [shoeInfo, shoeState] = useState(2);
-  let [shoes] = useState(data)
-  console.log(shoes[0].title)
-
   return (
     <div className="App">
-
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Brand href="#home">Shoe Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link onClick={()=> { navigate('/') }}>Home</Nav.Link>
-            <Nav.Link onClick={()=> { navigate('/detail') }}>Detail</Nav.Link>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-    
 
       <div className='main-bg'></div>
+
       <Container>
         <Row>
-          <Products shoes={shoes} />
-          <Products shoes={shoes} />
-          <Products shoes={shoes} />
+          <Col sm>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg'  width="80%" />
+            <h4>상품명</h4>
+            <p>상품 설명</p>
+          </Col>
+
+          <Col sm>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%" />
+            <h4>상품명</h4>
+            <p>상품 설명</p>
+          </Col>
+
+          <Col sm>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%" />
+            <h4>상품명</h4>
+            <p>상품 설명</p>
+          </Col>
+
         </Row>
-    </Container>
-
-
+      </Container>
 
     </div>
   );
 }
 
-function Products(props){
-  return (
-    <Col sm>
-        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-        <h4>{props.shoes[0].title}</h4>
-        <p>상품 설명: {props.shoes[0].content}</p>
-        <p>가격: {props.shoes[0].price}</p>
-    </Col>
-  )
-}
 
 export default App;
