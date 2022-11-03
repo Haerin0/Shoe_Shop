74b964bd39ddd876852825a8083cd8d748f6d2c5
Herin Park <parkhaerin@gmail.com>
2022-11-03 09:52:53 +0900

    timer added

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,25 +1,49 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-function Detail(props) {
+
+function Detail(props){
+
+    useEffect(()=> {
+        let a = setTimeout(()=>{ 
+            setAlert(false)
+        }, 2000)
+        return ()=> {
+            clearTimeout(a)
+        }
+    },[]) //mount 시 1회만 동작하게
 
     let { id } = useParams();
-    // console.log(id);
+    let found = props.shoes.find(function(x){
+      return x.id == id
+    });
 
+    let [count, setCount] = useState(0)
+    let [alert, setAlert] = useState(true)
+  
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-                </div>
-                <div className="col-md-6">
-                <h4 className="pt-5">{props.shoes[id].title}</h4>
-                <p>{props.shoes[id].content}</p>
-                <p>{props.shoes[id].price}</p>
-                <button className="btn btn-danger">주문하기</button> 
-                </div>
-            </div>
-        </div> 
+      <div className="container">
+        {
+            alert == true
+            ? <div className="alert alert-warning">
+            2초 이내 구매시 할인
+        </div> : null
+        }
+        { count }
+        <button onClick={()=>{ setCount(count + 1) }}>Button</button>
+        <div className="row">
+          <div className="col-md-6">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          </div>
+          <div className="col-md-6 mt-4">
+            <h4 className="pt-5">{found.title}</h4>
+            <p>{found.content}</p>
+            <p>{found.price}원</p>
+            <button className="btn btn-danger">주문하기</button> 
+          </div>
+        </div>
+    </div>  
     )
-}
-
-export default Detail;
+  };
+  
+  export default Detail 
